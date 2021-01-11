83ba40e50187ccc4a987db28f586bb6ba62cae71
phamquyetthang <quyetthang.phucchi@gmail.com>
2021-01-11 14:00:07 +0700

    env,log

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,42 @@
 import "dotenv/config";
-import { ROUTE } from "core/interfaces";
+import { ROUTE } from "./core/interfaces";
 import express from "express";
 import mongoose from "mongoose";
+import hpp from "hpp";
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors";
+import { Logger } from "./core/utils";
 export default class App {
   public app: express.Application;
   public port: string | number;
+  public production: boolean;
+
   constructor(routes: ROUTE[]) {
     this.app = express();
     this.port = process.env.PORT || 5000;
+    this.production = process.env.NODE_ENV == "production" ? true : false;
     this.initializeRoutes(routes);
     this.connectToDatabase();
+    this.initializeMiddleware();
   }
 
   public listen() {
     this.app.listen(this.port, () => {
-      console.log(`Server is listening on port : ${this.port}`);
+      Logger.info(`Server is listening on port : ${this.port}`);
     });
+  }
+
+  private initializeMiddleware() {
+    if (this.production) {
+      this.app.use(hpp());
+      this.app.use(helmet());
+      this.app.use(morgan("combined"));
+      this.app.use(cors({ origin: "youdomain.com", credentials: true }));
+    } else {
+      this.app.use(morgan("dev"));
+      this.app.use(cors({ origin: true, credentials: true }));
+    }
   }
 
   private initializeRoutes(routes: ROUTE[]) {
@@ -25,22 +46,20 @@ export default class App {
   }
 
   private connectToDatabase() {
-    try {
-      const connectString = process.env.MONGODB_URI;
-      if (!connectString) {
-        console.log("connectString invalid");
-        return;
-      } else {
-        mongoose.connect(connectString, {
+    const connectString = process.env.MONGODB_URI;
+    if (!connectString) {
+      Logger.error("connectString invalid");
+      return;
+    } else {
+      mongoose
+        .connect(connectString, {
           useNewUrlParser: true,
           useUnifiedTopology: true,
           useFindAndModify: false,
           useCreateIndex: true,
-        });
-        console.log("Database connect successfully! ");
-      }
-    } catch (error) {
-      console.log("Connect to database error: " + error);
+        })
+        .then(() => Logger.info("Database connect successfully! "))
+        .catch((err) => Logger.error(err));
     }
   }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
+import { validateEnv } from "./core/utils";
 import App from "./app";
 import { InderRoutes } from "./modules/index";
+
+validateEnv();
 
 const routes = [ new InderRoutes() ]
 const app = new App(routes);
