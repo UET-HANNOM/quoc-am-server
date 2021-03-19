d4fbe908c471b695548bc05e6763acabf24dc510
phamquyetthang <quyetthang.phucchi@gmail.com>
2021-03-19 16:33:40 +0700

    Tạo API login cho user

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import AuthRoutes from "@modules/auth/routes";
 import { validateEnv } from "@core/utils";
 import { InderRoutes } from "@modules/index";
 import UsersRoutes from "@modules/users/routes";
@@ -5,6 +6,6 @@ import App from "./app";
 
 validateEnv();
 
-const routes = [ new InderRoutes() , new UsersRoutes()]
+const routes = [new InderRoutes(), new UsersRoutes(), new AuthRoutes()];
 const app = new App(routes);
 app.listen();
