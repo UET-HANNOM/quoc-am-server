5e30bf78b187e1fba6934cfb589cdc9c8a11aa50
phamquyetthang <43201625+phamquyetthang@users.noreply.github.com>
2021-04-19 23:08:44 +0700

    fix cors bug

--- src/app.ts
+++ src/app.ts
@@ -34,7 +34,7 @@ export default class App {
       this.app.use(hpp());
       this.app.use(helmet());
       this.app.use(morgan("combined"));
-      this.app.use(cors({ origin: "youdomain.com", credentials: true }));
+      this.app.use(cors({ origin: true, credentials: true }));
     } else {
       this.app.use(morgan("dev"));
       this.app.use(cors({ origin: true, credentials: true }));
