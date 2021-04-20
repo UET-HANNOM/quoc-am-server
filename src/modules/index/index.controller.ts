0b1bbc44d0976af923e0895732f64fc8d95c79dc
phamquyetthang <43201625+phamquyetthang@users.noreply.github.com>
2021-04-20 13:33:54 +0700

    add sample data

--- src/modules/index/index.controller.ts
+++ src/modules/index/index.controller.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from "express";
-
+const textData = require("./sampleData.json");
 export default class IndexController {
   public index = (req: Request, res: Response, next: NextFunction) => {
     try {
@@ -11,10 +11,7 @@ export default class IndexController {
   };
   public sampleData = (req: Request, res: Response, next: NextFunction) => {
     try {
-      console.log(req.body);
-      let text =
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
-      res.status(200).json(text);
+      res.status(200).json(textData);
     } catch (err) {
       next(err);
     }
